1ff505d82346fc1d144cdf5276cdcdc360544b05
Ravi Garbuja Pun <-v>
2019-08-31 22:25:28 +0545

    test: Passing test for CategoryDao class
    - Insert and Get single Category
    - Insert and Get multiple Categories
    - Pre Populate Category table from SampleCategories

---
+++ app/src/androidTest/java/com/example/crs_ro/data/LiveDataTestUtil.kt
@@ -0,0 +1,30 @@
+package com.example.crs_ro.data
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
+
+/**
+ * Get the value from a LiveData object. We're waiting for LiveData to emit, for 2 seconds.
+ * Once we got a notification via onChanged, we stop observing.
+ *
+ * This Utility is an extension function as we are extending upon on the LiveData<T> class
+ */
+@Throws(InterruptedException::class)
+fun <T> LiveData<T>.waitForValue(): T {
+    val data = arrayOfNulls<Any>(1)
+    val latch = CountDownLatch(1)
+    val observer = object : Observer<T> {
+        override fun onChanged(o: T?) {
+            data[0] = o
+            latch.countDown()
+            this@waitForValue.removeObserver(this)
+        }
+    }
+    this.observeForever(observer)
+    latch.await(2, TimeUnit.SECONDS)
+
+    return data[0] as T
+}
---
+++ app/src/androidTest/java/com/example/crs_ro/data/category/CategoryDaoTest.kt
@@ -0,0 +1,80 @@
+package com.example.crs_ro.data.category
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.core.util.forEach
+import androidx.room.Room
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import com.example.crs_ro.data.AppDatabase
+import com.example.crs_ro.data.waitForValue
+import kotlinx.coroutines.runBlocking
+import org.junit.*
+import org.junit.runner.RunWith
+import java.io.IOException
+
+@RunWith(AndroidJUnit4::class)
+class CategoryDaoTest {
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var categoryDao: CategoryDao
+    private lateinit var db: AppDatabase
+
+    @Before
+    fun createDb() {
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        // Using an in-memory database because the information stored here disappears when the
+        // process is killed.
+        db = Room.inMemoryDatabaseBuilder(context, AppDatabase::class.java)
+            // Allowing main thread queries, just for testing.
+            .allowMainThreadQueries()
+            .build()
+        categoryDao = db.categoryDao()
+    }
+
+    @After
+    @Throws(IOException::class)
+    fun closeDb() {
+        db.close()
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun insertAndGetCategory() = runBlocking {
+        //create a category in our table
+        val category = Category(1, "Tops")
+        categoryDao.insert(category)
+        val allCategories = categoryDao.getAll().waitForValue()
+        //check if category just inserted is equal retrieved from the table
+        Assert.assertEquals(allCategories[0].name, category.name)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun getAllCategories() = runBlocking {
+        //insert two categories in table
+        val category1 = Category(1, "Tops")
+        categoryDao.insert(category1)
+        val category2 = Category(2, "Bottom")
+        categoryDao.insert(category2)
+        //get all categories from the table
+        val allCategories = categoryDao.getAll().waitForValue()
+        //check if they are equal to above inserted values
+        Assert.assertEquals(allCategories[0].name, category1.name)
+        Assert.assertEquals(allCategories[1].name, category2.name)
+    }
+
+    //Test pre populating data
+    @Test
+    @Throws(Exception::class)
+    fun prepopulateData() = runBlocking {
+        //TODO Test functionality to populate database from SampleCategories, retrieve all categories and check if they match
+        //insert from sample categories
+        categoryDao.insertAll(SampleCategories.getSampleCategories())
+        //get all categories from the table
+        val allCategories = categoryDao.getAll().waitForValue()
+        //check if they are equal
+        Assert.assertEquals(allCategories, SampleCategories.getSampleCategories())
+    }
+
+}
