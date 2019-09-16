de3c688629e9b942447a1fbf51362109fbe7e923
Ravi Garbuja Pun <-v>
2019-09-16 15:09:13 +0545

    fix: Failing build due to not indexing a foreign key
    category_id field in SubCategory needs to be indexed because it is a foreign key which may cause a full table lookup on trying to access.
    
    closes #17

--- app/src/main/java/com/example/crs_ro/data/subcategory/SubCategory.kt
+++ app/src/main/java/com/example/crs_ro/data/subcategory/SubCategory.kt
@@ -1,20 +1,22 @@
 package com.example.crs_ro.data.subcategory
 
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.ForeignKey
-import androidx.room.PrimaryKey
+import androidx.room.*
 import com.example.crs_ro.data.category.Category
 
 /**
  * One to many Relationship from Category to SubCategory
  */
 @Entity(
-    tableName = "sub_categories", foreignKeys = arrayOf(
+    tableName = "sub_categories",
+    foreignKeys = arrayOf(
         ForeignKey(
             entity = Category::class,
             parentColumns = arrayOf("id"),
             childColumns = arrayOf("category_id")
+        )
+    ), indices = arrayOf(
+        Index(
+            value = ["category_id"]
         )
     )
 )
